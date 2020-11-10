0066cb801339bbdc8c5309ff3bad36ebf01af084
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2020-11-11 09:58:49 +1000

    Library and Intial Commands Upload

---
+++ Main.py
@@ -0,0 +1,25 @@
+import time,os
+def KillTask(task):
+    print("Subprocess: Killing " + task +".exe")
+    time.sleep(int(ReadSettings()[3]))
+    os.system("taskkill /f /im " + task + ".exe")
+
+def OpenSettings(sub):
+    print("Subprocess: Opening Settings")
+    os.system("start ms-settings:" + sub)
+
+def BingSearch(Query):
+    return('https://www.bing.com/search?q=' + str(Query))
+
+def ReadSettings():
+    file1 = open('settings.txt', 'r')
+    Lines = file1.readlines()
+
+    Settings = [1]*len(Lines)
+    count = 0
+    for line in Lines:
+        if(line.startswith("#")): 
+            continue
+        Settings[count] = line.strip()
+        count += 1
+    return Settings
---
+++ Search.py
@@ -0,0 +1,24 @@
+print("Libraries: Importing")
+
+import webbrowser as edge
+import Main
+    
+print("Libraries: Successfully Imported")
+
+Main.OpenSettings(Main.ReadSettings()[4])
+input("User: Please change your deafault browser to Microsoft Edge")
+
+
+#Main Loop
+Main.KillTask(Main.ReadSettings()[2])
+for TabRound in range(0, int(Main.ReadSettings()[0]), 1):
+    for searching in range(0,int(Main.ReadSettings()[1]),1):
+        
+        print("Tab: Opening, " + str(searching * (TabRound + 1)))
+        #Create a unique Search
+        edge.open(Main.BingSearch((searching + 1) * (TabRound + 1)))
+    Main.KillTask("msedge")
+edge.open('https://account.microsoft.com/rewards/')
+
+Main.OpenSettings(Main.ReadSettings()[4])
+input("User: You may now set your default browser back")
