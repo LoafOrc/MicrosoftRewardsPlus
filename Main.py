7b69dc2f5f8c67635fce2903716d18c18e7bb52f
LoafOrc <rixonlogan@hotmail.com>
2020-11-12 08:40:42 +1000

    Optimisation
    Removed the fact that every time we were calling for a setting we were reading all settings. It is now stored in a variable. DependecyCheck has been moved to the library to cut down on libraries imported in the search.py file

--- Main.py
+++ Main.py
@@ -1,22 +1,14 @@
-import time,os
+import time,os,subprocess,sys
+
+def DependencyCheck(module):
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+    if (module in installed_packages) == False:
+        os.system("pip install " + module)
 
 #Check Updates
 from update_check import checkForUpdates
-
-checkForUpdates(__file__, "https://raw.githubusercontent.com/LoafOrc/MicrosoftRewardsPlus/" + ReadSettings()[5] + "/Main.py")
-
-
-def KillTask(task):
-    print("Subprocess: Killing " + task +".exe")
-    time.sleep(int(ReadSettings()[3]))
-    os.system("taskkill /f /im " + task + ".exe")
-
-def OpenSettings(sub):
-    print("Subprocess: Opening Settings")
-    os.system("start ms-settings:" + sub)
-
-def BingSearch(Query):
-    return('https://www.bing.com/search?q=' + str(Query))
 
 def ReadSettings():
     file1 = open('settings.txt', 'r')
@@ -30,3 +22,19 @@ def ReadSettings():
         Settings[count] = line.strip()
         count += 1
     return Settings
+
+Settings = ReadSettings()
+
+checkForUpdates(__file__, "https://raw.githubusercontent.com/LoafOrc/MicrosoftRewardsPlus/" + Settings[5] + "/Main.py")
+
+def KillTask(task):
+    print("Subprocess: Killing " + task +".exe")
+    time.sleep(int(Settings[3]))
+    os.system("taskkill /f /im " + task + ".exe")
+
+def OpenSettings(sub):
+    print("Subprocess: Opening Settings")
+    os.system("start ms-settings:" + sub)
+
+def BingSearch(Query):
+    return('https://www.bing.com/search?q=' + str(Query))
--- Search.py
+++ Search.py
@@ -1,34 +1,27 @@
 #Check Updates
 
-import subprocess, sys, os
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
-
-if ('update_check' in installed_packages) == False:
-    os.system("pip install update_check")
-
-from update_check import checkForUpdates
 
 
 import webbrowser as edge
 import Main
+Main.DependencyCheck('update_check')
 
-checkForUpdates(__file__, "https://raw.githubusercontent.com/LoafOrc/MicrosoftRewardsPlus/" + Main.ReadSettings()[5] + "/Search.py")
+from update_check import checkForUpdates
+checkForUpdates(__file__, "https://raw.githubusercontent.com/LoafOrc/MicrosoftRewardsPlus/" + Main.Settings[5] + "/Search.py")
 
-Main.OpenSettings(Main.ReadSettings()[4])
+Main.OpenSettings(Main.Settings[4])
 input("User: Please change your deafault browser to Microsoft Edge")
 
 
 #Main Loop
-Main.KillTask(Main.ReadSettings()[2])
-for TabRound in range(0, int(Main.ReadSettings()[0]), 1):
-    for searching in range(0,int(Main.ReadSettings()[1]),1):
+Main.KillTask(Main.Settings[2])
+for TabRound in range(0, int(Main.Settings[0]), 1):
+    for searching in range(0,int(Main.Settings[1]),1):
         
         #Create a unique Search
         edge.open(Main.BingSearch((searching + 1) * (TabRound + 1)))
     Main.KillTask("msedge")
 edge.open('https://account.microsoft.com/rewards/')
 
-Main.OpenSettings(Main.ReadSettings()[4])
+Main.OpenSettings(Main.Settings[4])
 input("User: You may now set your default browser back")
