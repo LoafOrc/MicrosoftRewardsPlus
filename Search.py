1206d2af45a33f2e0e58b7f46774fce6482b24c9
LoafOrc <ltr378@gmail.com>
2021-08-20 08:57:58 +1000

    no longer need to switch browsers
    WHY DOESN'T IT IGNORE SETTINGS.TXT HELP

--- Search.py
+++ Search.py
@@ -4,15 +4,12 @@
 
 import webbrowser as edge
 import Main
+import os
 Main.DependencyCheck('update_check')
 
 from update_check import checkForUpdates
 if(Main.Settings[5] != "none"):
     checkForUpdates(__file__, "https://raw.githubusercontent.com/LoafOrc/MicrosoftRewardsPlus/" + Main.Settings[5] + "/Search.py")
-
-Main.OpenSettings(Main.Settings[4])
-input("User: Please change your default browser to Microsoft Edge")
-
 
 #Main Loop
 Main.KillTask(Main.Settings[2])
@@ -20,9 +17,10 @@ for TabRound in range(0, int(Main.Settings[0]), 1):
     for searching in range(0,int(Main.Settings[1]),1):
         
         #Create a unique Search
-        edge.open(Main.BingSearch(searching + TabRound*int(Main.Settings[1])))
+        query = Main.BingSearch(searching + TabRound*int(Main.Settings[1]))
+        os.system("start msedge " + query)
     Main.KillTask("msedge")
-edge.open('https://account.microsoft.com/rewards/')
+os.system("start msedge https://account.microsoft.com/rewards/")
 
-Main.OpenSettings(Main.Settings[4])
+
 input("User: You may now set your default browser back")
